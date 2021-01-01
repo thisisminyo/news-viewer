6f17ad9b1cb2e7b2d1120641a02e8228e13781a1
minyo <005snail@gmail.com>
2021-01-01 13:30:33 +0900

    new components - newsItem, NewsList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=8053640cd13046c8936495e84c0d07cc',
       );
       setData(response.data);
     } catch (e) {
@@ -15,18 +16,19 @@ const App = () => {
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <NewsList />
+    // <div>
+    //   <div>
+    //     <button onClick={onClick}>불러오기</button>
+    //   </div>
+    //   {data && (
+    //     <textarea
+    //       rows={7}
+    //       value={JSON.stringify(data, null, 2)}
+    //       readOnly={true}
+    //     />
+    //   )}
+    // </div>
   );
 };
 
